34b341ed4d86caad695baec1e45dbfdc6f04c5e5
unknown <emmanuelkadosh91@gmail.com>
2021-10-14 15:34:05 -0300

    Zoando

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,21 @@ function Contador() {
         <div>
             <div>{contador}</div>
             <button onClick={incContador}>Incrementar</button>
+            <button onClick={Resultado}>Resultado</button>
         </div>
     )
 
     function incContador() {
         setContador(contador+1)
+    }
+
+    function Resultado() {
+        return (
+            <div>
+                <h2>Quantas vezes Luciano Comeu Ricardo: </h2>
+                <h3>{contador}</h3>
+            </div>
+        )
     }
 }
 
